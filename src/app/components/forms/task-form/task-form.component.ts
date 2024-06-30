d738c5693dd58dc7f9c0315ac00b8e8d04767695
kaoutar <kaoutar1301@gmail.com>
2024-06-30 22:34:59 +0200

    VersionFinal

--- src/app/components/forms/task-form/task-form.component.ts
+++ src/app/components/forms/task-form/task-form.component.ts
@@ -7,21 +7,36 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { ApiService } from '../../../services/service-api.service';
-import { environment } from '../../../../environments/environment';
+import { StoreService } from '../../../services/store.service';
+import { format } from 'date-fns';
+import { CommonModule } from '@angular/common';
+import { ModalService } from '../../../services/modal-service.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
 })
 export class TaskFormComponent implements OnInit {
   taskForm: FormGroup = new FormGroup({});
+  formType: string = 'Task';
+  listid: number = 0;
+  isUpdateValue: boolean = false;
 
   @Input() currentId: number = 0;
 
-  constructor(private fb: FormBuilder, private apiService: ApiService) {}
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private storeService: StoreService,
+    private modalService: ModalService,
+    private snackBar: MatSnackBar, 
+  ) {}
+
+
 
   ngOnInit(): void {
     this.taskForm = this.fb.group({
@@ -29,6 +44,59 @@ export class TaskFormComponent implements OnInit {
       Categorie: ['', Validators.required], // Added taskCategorie form control
       Description: ['', Validators.required], // Added taskDescription form control
       dueDate: ['', Validators.required],
+    });
+
+    this.storeService.taskId$.subscribe((taskId: number | null) => {
+      if (taskId !== null) {
+        this.currentId = taskId;
+        // Perform actions when taskId changes, if needed
+      } else {
+        console.log('TaskId is null');
+        // Handle the case where taskId is null
+      }
+    });
+
+    this.storeService.listId$.subscribe((listId: number | null) => {
+      if (listId !== null) {
+        this.listid = listId;
+        // Perform actions when listId changes, if needed
+      } else {
+        console.log('ListId is null');
+        // Handle the case where listId is null
+      }
+    });
+
+
+    this.isUpdateValue = this.storeService.isUpdate;
+    // Use taskId from StoreService as the currentId
+  }
+
+  edit(formType: string) {
+    const formattedDueDate = format(
+      this.taskForm.value.dueDate,
+      "yyyy-MM-dd'T'00:00:00"
+    );
+    // Create a payload with the form values and add the id as a key
+    const payload = {
+      ...this.taskForm.value,
+      dueDate: formattedDueDate,
+      id: this.currentId,
+      listid: this.listid, // Add the id to the payload
+    };
+
+    console.log(payload);
+
+    this.apiService.update(this.currentId, payload, formType).subscribe({
+      next: (task) => {
+        if (task !== null) {
+          this.taskForm.patchValue(task);
+          console.log('Task details fetched for editing:', task);
+        } else {
+          console.log('Task is null');
+        }
+        // Emit the taskCreated event immediately after handling the response
+        this.apiService.taskCreated();
+      },
     });
   }
 
@@ -38,18 +106,19 @@ export class TaskFormComponent implements OnInit {
         ...this.taskForm.value,
         listId: this.currentId, // Add the projectId field with currentId value
       };
-
       console.log('Payload:', payload);
-      this.apiService
-        .post(`${environment.apiUrl}/api/task`, payload)
-        .subscribe({
-          next: (response) => {
-            console.log('Success:', response);
-            // Assuming there's a method to handle task creation event
-            this.apiService.taskCreated(); // Publish the event
-          },
-          error: (error) => console.error('Error:', error),
-        });
+      this.apiService.post(payload, this.formType).subscribe({
+        next: (response) => {
+          console.log('Success:', response);
+          this.apiService.taskCreated();
+          this.modalService.close();
+          this.snackBar.open('Task created successfully!', 'Close', {
+            duration: 3000,
+            panelClass: ['custom-snackbar'] // Duration in milliseconds after which the snack-bar will be automatically dismissed.
+          }); //// Emit the task creation event
+        },
+        error: (error) => console.error('Error:', error),
+      });
     }
   }
 }
--- src/app/tasks/task-card/task-card.component.ts
+++ src/app/tasks/task-card/task-card.component.ts
@@ -1,64 +1,141 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ApiService } from '../../services/service-api.service';
-import { environment } from '../../../environments/environment';
+import { StoreService } from '../../services/store.service';
 import { CommonModule } from '@angular/common';
 import { Comments } from '../../models/comment';
+import { FrenchDatePipe } from '../../pipes/french-date.pipe';
+import { ModalService } from '../../services/modal-service.service';
+import { Subscription } from 'rxjs';
+import { CommentComponent } from '../../comments/comment/comment.component';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss'],
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FrenchDatePipe,CommentComponent, MatIconModule ],
   standalone: true,
 })
 export class TaskCardComponent implements OnInit {
-  @Input() taskId?: string | number = '';
+  @Input() taskId: number = 0;
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() comments: Comments[] = [];
   @Input() Listid: number = 0;
+  @Input() dueDate: string = '';
+  @Input() projectid: number = 0;
+ @Input() taskCategorie: string = '';
 
   isVisible: boolean = false;
+  showAddCommentButton: boolean = true;
   showComments: boolean = false;
   commentForm: FormGroup = new FormGroup({});
+  formType: string = 'Comment';
+  private modalSubscription: Subscription = new Subscription();
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private storeService: StoreService,
+    private modalService: ModalService
+  ) {}
 
-  constructor(private fb: FormBuilder, private apiService: ApiService) {}
+  getBackgroundColor(categorie: string): string {
+    switch (categorie) {
+      case 'Marketing':
+        return '#30e49f';
+      case 'Design':
+        return '#FF584D';
+      case 'Informatique':
+        return '#FED440';
+      default:
+        return '#FFBBAB'; // Default color
+    }
+  }
 
   ngOnInit(): void {
     this.commentForm = this.fb.group({
       Text: ['', Validators.required],
       // You can add more fields here as needed
     });
+    this.modalSubscription = this.modalService.watch().subscribe((status) => {
+      // Assuming the modal service has a method 'watch' that notifies about modal status
+      console.log(`Modal status: ${status}`);
+    });
   }
 
-  toggleComments(): void {
+  editTask(id: number) {
+    this.storeService.taskId = id;
+    this.storeService.listId = this.Listid;
+    
+    console.log(`Editing task with id: ${id}`);
+
+    this.storeService.isUpdate = true;
+
+    this.toggleModal('taskform', id, this.storeService.taskId);
+
+  }
+
+  deleteTask(id: number) {
+    console.log('Taskid: ', this.taskId);
+
+    // Step 1: Find the index of the list to delete
+
+    // call api service  to delete with this id
+    this.apiService.delete(id, 'task').subscribe(() => {
+      this.apiService.taskCreated();
+    });
+  }
+
+  toggleModal(action: string, projectId: number, data: any) {
+    // Assuming the modal service has an 'open' method that can be used to open a modal
+    this.modalService.open(action, projectId, data);
+  }
+
+  deleteComment = (id: number) => {
+    // Assuming the modal service has a 'confirm' method that can be used to show a confirmation dialog
+ 
+  }
+
+  editComment = (id: number) => {
+
+  }
+
+  toggleComments() {
     this.showComments = !this.showComments;
+    // Assuming showComments is true when comments are shown
+    this.showAddCommentButton = !this.showComments;
   }
-
   showCommentsList(): void {
     this.isVisible = !this.isVisible;
   }
-
   submitComment(): void {
+    
     if (this.commentForm.valid) {
       const payload = {
         ...this.commentForm.value,
         taskId: this.taskId,
-        author : "Anonymous" // Assuming taskId is needed for the comment
+        author: 'Anonymous', // Assuming taskId is needed for the comment
         // Add any other relevant fields here
       };
 
       console.log('Comment Payload:', payload);
-      this.apiService.post(`${environment.apiUrl}/api/comment`, payload).subscribe({
+      this.apiService.post(payload, this.formType).subscribe({
         next: (response) => {
           console.log('Comment Success:', response);
           // Handle successful comment submission (e.g., refresh comments list)
+          this.apiService.commentCreated(); // Notify the ApiService that a comment has been created
         },
         error: (error) => console.error('Comment Error:', error),
       });
     }
-  }
 
+  }
   // Methods for calculating card height remain unchanged
 }
